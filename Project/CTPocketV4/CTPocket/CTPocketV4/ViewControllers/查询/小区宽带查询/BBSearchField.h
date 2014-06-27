58ebb7c583d94edc8631b0bef6b843d03f6d61a0
jack <john@gmail.com>
2014-06-27 15:16:25 +0800

    4.3.0.40

---
+++ Project/CTPocketV4/CTPocket/CTPocketV4/ViewControllers/查询/小区宽带查询/BBSearchField.h
@@ -0,0 +1,13 @@
+//
+//  BBSearchField.h
+//  CTPocketV4
+//
+//  Created by Gong Xintao on 14-6-20.
+//  Copyright (c) 2014年 广东亿迅科技有限公司. All rights reserved.
+//  创建左边距为10像素的输入框
+
+#import <UIKit/UIKit.h>
+
+@interface BBSearchField : UITextField
+
+@end
